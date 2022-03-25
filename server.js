56877ba5f55a251623c03a342b8f2b7d4112ca63
Colin <colinhickey@mac.com>
2022-03-25 16:20:47 -0400

    passed tests

--- server.js
+++ server.js
@@ -2,48 +2,62 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 var moment = require("moment");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
+//timestamp endpoint
 app.get("/api/:date", function (req, res) {
-  let date = moment(req.params.date);
-  //console.log(moment(date).format("x"));
-  //console.log(moment(date).format("ddd MMM YYYY HH:mm:ss z"));
-  let utc = moment(date).format("ddd, MMM YYYY HH:mm:ss z") + "GMT";
-  if (date.isValid()) {
+  const re = /^[0-9]/;
+  const date = moment(req.params.date);
+  const utc = moment(date).format("ddd, DD MMM YYYY HH:mm:ss z") + "GMT";
+  const dateInt = parseInt(req.params.date);
+  const utcInt = moment(dateInt).format("ddd, DD MMM YYYY HH:mm:ss z") + "GMT";
+
+  if (moment(date).isValid()) {
     res.json({
-      unix: moment(date).format("x"),
-      utc: utc
-      //Fri, 25 Dec 2015 00:00:00 GMT
+      unix: parseInt(moment(date).format("x")),
+      utc: utc,
+    });
+  } else if (re.test(dateInt) == true) {
+    
+    res.json({
+      unix: parseInt(moment(dateInt).format("x")),
+      utc: utcInt,
     });
   } else {
     res.json({ error: "Invalid Date" });
   }
+});
+
+app.get("/api/", function (req, res) {
+  let now = Date.now();
+  let utcNow = moment(now).format("ddd, DD MMM YYYY HH:mm:ss z") + "GMT";
+  res.json({
+    unix: parseInt(moment(now).format("x")),
+    utc: utcNow,
+  });
 });
